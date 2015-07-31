5ca40af689f9b25d6b23ad379b284dad375bdd58
Vincent Kollin <vkollin@gmail.com>
2015-07-31 19:54:06 +0200

    PSR-4

--- src/Beije/Cameraraw/Cameraraw.php
+++ src/Beije/Cameraraw/Cameraraw.php
@@ -3,23 +3,23 @@
 /**
 * A class that can fetch preview images from camera raw files.
 *
-* In order to use this class you'll need a few php mods and 
+* In order to use this class you'll need a few php mods and
 * some other software installed on your server.
 *
 * - imagemagick (Manage images)
 * - ufraw (Reading and converting raw files)
 * - exiv2 (Extracting previews from raw files)
-* 
+*
 */
 
-namespace Beije\CameraRaw
+namespace Beije\Cameraraw
 
-class CameraRaw {
+class Cameraraw {
 	/**
 	* @var	array	an array containing popular raw file format extensions
-	*/	
+	*/
 	private static $rawExtensions = array('.ari','.arw','.bay','.crw','.cr2','.cap','.dcs','.dcr','.dng','.drf','.eip','.erf','.fff','.iiq','.k25','.kdc','.mdc','.mef','.mos','.mrw','.nef','.nrw','.obm','.orf','.pef','.ptx','.pxn','.r3d','.raf','.raw','.rwl','.rw2','.rwz','.sr2','.srf','.srw', '.x3f');
-	
+
 	/**
 	* Checks if a file exists based on the filepath.
 	*
@@ -87,8 +87,8 @@ class CameraRaw {
 
 		$im = new \Imagick($sourceFilePath);
 		$im->setImageFormat('jpg');
-		$im->setImageCompressionQuality($quality); 
-    		$im->stripImage(); 
+		$im->setImageCompressionQuality($quality);
+    		$im->stripImage();
 		$im->thumbnailImage($width, $height, true);
 		$im->writeImage($targetFilePath);
 		$im->clear();
@@ -128,11 +128,11 @@ class CameraRaw {
 		$previewNumber = intval($previewNumber);
 		$filename = pathinfo($sourceFilePath, PATHINFO_FILENAME);
 		$return = array();
-		
+
 		// generate the preview with exiv2, save it to the temp directory
 		exec('exiv2 -ep'.$previewNumber.' -l '.sys_get_temp_dir(). ' "' . $sourceFilePath . '"', $return);
 		$previewFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $filename . '-preview' . $previewNumber . '.jpg';
-		
+
 		// check that the preview got saved correctly
 		if(!self::checkFile($previewFilePath)) {
 			throw new \Exception('No preview file.');
